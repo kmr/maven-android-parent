dbd4624a1c9670ac8071077e629e5e8e3a2b053b
kmr <kmr.clover@gmail.com>
2009-12-02 23:35:43 +0900

    initial commit

---
+++ src/main/java/net/necomimi/App.java
@@ -0,0 +1,13 @@
+package net.necomimi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
